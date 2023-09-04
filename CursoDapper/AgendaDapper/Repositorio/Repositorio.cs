88b5861887307c12ec18f02f0339d1e2a65f8499
Carlos Perez Salazar <cps172779@gmail.com>
2023-09-04 16:35:30 -0600

    Create interface and respository pt1

---
+++ CursoDapper/AgendaDapper/Repositorio/Repositorio.cs
@@ -0,0 +1,46 @@
+﻿using AgendaDapper.Models;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AgendaDapper.Repositorio
+{
+    public class Repositorio : IRepositorio
+    {
+        private readonly IDbConnection _bd;
+
+        public Repositorio(IConfiguration configuration)
+        {
+            _bd = new SqlConnection(configuration.GetConnectionString("ConexionSQLLocalDB"));
+        }
+
+        public Cliente ActualizarCliente(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente AgregarCliente(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BorrarCliente(int id)
+        {
+            var sql = "DELETE FROM Cliente WHERE IdCliente = @IdCliente";
+            _bd.Execute(sql, new {@IdCliente = id});
+        }
+
+        public Cliente GetCliente(int id)
+        {
+            var sql = $"SELECT * FROM Cliente WHERE IdCliente = @IdCliente";
+            return _bd.Query<Cliente>(sql, new { @IdCliente = id }).SingleOrDefault();
+        }
+
+        public List<Cliente> GetClientes()
+        {
+            var sql = "SELECT * FROM Cliente";
+
+            return _bd.Query<Cliente>(sql).ToList(); ;
+        }
+    }
+}
