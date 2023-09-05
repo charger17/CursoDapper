4e4523fbbc16b8382c38694bfcb29d945de17515
Carlos Perez Salazar <cps172779@gmail.com>
2023-09-05 14:02:51 -0600

    Configure by area

--- CursoDapper/BlogDapper/Areas/Front/Controllers/InicioController.cs
+++ CursoDapper/BlogDapper/Areas/Front/Controllers/InicioController.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BlogDapper.Controllers
+namespace BlogDapper.Areas.Front.Controllers
 {
-    public class HomeController : Controller
+    [Area("Front")]
+    public class InicioController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<InicioController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public InicioController(ILogger<InicioController> logger)
         {
             _logger = logger;
         }
