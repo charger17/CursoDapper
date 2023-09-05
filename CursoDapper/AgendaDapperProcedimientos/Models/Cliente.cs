ba888ef0d19da1d63abcc45326e34645ef4873e3
Carlos Perez Salazar <cps172779@gmail.com>
2023-09-04 21:02:44 -0600

    Add new project for sp

---
+++ CursoDapper/AgendaDapperProcedimientos/Models/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaDapper.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+
+        [Required]
+        public string Nombres { get; set; }
+
+        [Required]
+        public string Apellidos { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public int Telefono { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Pais { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Creación")]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
