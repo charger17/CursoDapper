66d60533967978963428c1e2eb5c7262b82bb69f
Carlos Perez Salazar <cps172779@gmail.com>
2023-09-08 18:14:26 -0600

    Accesos controler and login view

---
+++ CursoDapper/BlogDapper/Areas/Front/Controllers/AccesosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BlogDapper.Areas.Front.Controllers
+{
+    [Area("Front")]
+    public class AccesosController : Controller
+    {
+
+        private readonly IDbConnection _bd;
+        public AccesosController(IConfiguration configuration)
+        {
+            _bd = new SqlConnection(configuration.GetConnectionString("ConexionSQLLocalDB"));
+        }
+
+        [HttpGet]
+        public IActionResult Acceso()
+        {
+            return View();
+        }
+    }
+}
